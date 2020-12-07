ba232c995d1231bbe09050d475db2d9d7195f98a
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2020-12-07 08:55:43 +0100

    working filter for strings and character arrays

--- code_getter/get_extra.py
+++ code_getter/get_extra.py
@@ -1,6 +1,16 @@
 import re
 import sublime
 
+def filter(self,string):
+    
+    qm = ["\"","\'"]
+
+    for i in range(2): #colapse strings and char. arrays
+        string = re.sub(r"(\W){0}[^{0}]*{0}".format(qm[i]),r"\1{0}{0}".format(qm[i]),string)
+
+    print(string)
+    return string
+
 def nested_skip( self,s, start,end ):
     view = self.view
     row = view.rowcol(s.begin())[0]
@@ -10,8 +20,12 @@ def nested_skip( self,s, start,end ):
     while row <= lastrow:
 
         line = view.line(view.text_point(row, 0))
-        level += len(re.findall(r"(?:{})".format(start),view.substr(line)))
-        level -= len(re.findall(r"(?:{})".format(end),view.substr(line)))
+        line_string = view.substr(line)
+
+        line_string = filter(self,line_string)
+        
+        level += len(re.findall(r"(?:{})".format(start),line_string))
+        level -= len(re.findall(r"(?:{})".format(end),line_string))
         
         #print(row,": ",level)
         row += 1
@@ -26,19 +40,23 @@ def reversible_matching( self,s, up,dn, prefix="" ):
     view = self.view
     row = view.rowcol(s.begin())[0]
     lastrow = view.rowcol(view.size())[0]
-    level = 0
 
     if len(up) >= 2:
         up = "|".join(up)
     if len(dn) >= 2:
         dn = "|".join(dn)
-    
+
+    level = 0
     step = 0
     while ( row <= lastrow) & ( row >= 0 ):
 
         line = view.line(view.text_point(row, 0))
-        level += len(re.findall(r"{0}(?:{1})".format(prefix,up),view.substr(line)))
-        level -= len(re.findall(r"{0}(?:{1})".format(prefix,dn),view.substr(line)))
+        line_string = view.substr(line)
+        
+        line_string = filter(self,line_string)
+
+        level += len(re.findall(r"{0}(?:{1})".format(prefix,up),line_string))
+        level -= len(re.findall(r"{0}(?:{1})".format(prefix,dn),line_string))
         
         sign = (level > 0) - (level < 0)
         if sign:
